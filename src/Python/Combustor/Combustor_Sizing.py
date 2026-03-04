3f927dc9c1025dbc97eeaccc1b48b74583b77f73
rmcmahill <rowanmcmahill@gmail.com>
2026-03-03 21:22:07 -0500

    Value Change

--- src/Python/Combustor/Combustor_Sizing.py
+++ src/Python/Combustor/Combustor_Sizing.py
@@ -8,16 +8,16 @@ from src.Python.Combustor import CMB_Air_Distribution as air_dist
 
 t3 = 900 # K
 t4 = 1300 # K;
-tSecondaryDesired = 1800 # K
+tSecondary = 1800 # K
 tFlame = 2500 # K
-mDot3 = 50 # kg/s
+mDot3 = 33 # kg/s
 fuelAirRatio = 0.06381831
 cp3 = 1.124 # kJ/kg-K
 cpPrimary = 2.6 # kJ/kg-K
 
 # Calculates the air distribution to each section of the combustor
 # See CMB_Air_Distribution.py for more details on the function and its inputs/outputs
-pzd, szd, dzd = air_dist.Calc_Air_Distribution(t3, t4, tSecondaryDesired, tFlame, mDot3, fuelAirRatio, cp3, cpPrimary)
+pzd, szd, dzd = air_dist.Calc_Air_Distribution(t3, t4, tSecondary, tFlame, mDot3, fuelAirRatio, cp3, cpPrimary)
 
 print(f"Primary Zone Mass Flow Fraction: {pzd:.3f}")
 print(f"Secondary Zone Mass Flow Fraction: {szd:.3f}")
