ba3d988ddd275d5118a33fc0e24e1c8ff68b1891
rmcmahill <rowanmcmahill@gmail.com>
2026-07-19 18:35:43 -0400

    testing pyfluids

--- src/Python/Combustor/coolproptest.py
+++ src/Python/Combustor/coolproptest.py
@@ -1,6 +1,9 @@
 # 'pip install CoolProp' if not done already
 import CoolProp.CoolProp as CoolProp
 import CEA.CEA_Runner as CEA
+import numpy as np
+import pyfluids as pf
+from pyfluids import Mixture, FluidsList, Input
 
 # Create a simple linear mixing rule for the CO2 and Xenon pair
 ##CoolProp.apply_simple_mixing_rule('Ar', 'Xe', 'linear')
@@ -19,8 +22,13 @@ cp3             = 0.6 #params.cp3 # kJ/kg-
 
 data = CEA.Run_CEA(400, 300, 5.515806, 1) # K
 tPrim = data.t # temp of primary zone 
-cpPrimary = data.cp
 
+mixture = Mixture([FluidsList.Water, FluidsList.Ethanol], [60, 40]).with_state(
+    Input.pressure(200e3), Input.temperature(4)
+)
+print(mixture.density)  # 883.3922771627963
+
+"""
 # Pull the element types from data.prod_c and put them in a list
 element_types = []
 for comp in data.prod_c:
@@ -35,3 +43,4 @@ for i in range(len(element_types)):
     mixture += element_types[i] + '[' + str(element_percentages[i]) + ']'
     if i < len(element_types) - 1:
         mixture += '&'
+"""
