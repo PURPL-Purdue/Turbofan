52fa9b42d453f27770a1d74ed87b187543238d97
rmcmahill <rowanmcmahill@gmail.com>
2026-07-22 23:33:01 -0400

    Update Burner.py
    Added get_mixtures and fixed more nomenclature.

--- src/CMYK/Object_Definitions/Components/Burner.py
+++ src/CMYK/Object_Definitions/Components/Burner.py
@@ -28,7 +28,19 @@ class Burner(ComponentBase):
         self.Wfactor = None
         self.Wstream = None
 
-    def config(self) -> None:
+    def Get_Mixtures(fuel: str): #-> tuple[ceaMixture, ceaMixture, list]:
+        if fuel == 'Jet-A':
+            fuel_cea_name = 'Jet-A(L)'
+        else:
+            raise RuntimeError(f"Fuel '{fuel}' not supported. Please check spelling or use a different fuel.")
+        
+        reactant_names = [fuel_cea_name, 'Air']
+        #product_ceaMixture = ceaMixture(reactant_names, products_from_reactants = True)
+        #reactant_ceaMixture = ceaMixture(reactant_names)
+
+        return reactant_names
+
+    def Config(self) -> None:
         cfg = self.cfg[self.name]
         super()._set_Wfactor()
 
@@ -65,31 +77,31 @@ class Burner(ComponentBase):
         oxid = CEA.Oxidizer(name = "Air", temp = self.FlowIn.T0, wt = 100) #Oxidizer Specs
         fuel = CEA.Fuel(name = "Jet-A(L)", temp = self.FlowIn.T0, wt = 100) #Fuel Specs
 
-        def residual(FAR):
+        def Residual(FAR):
             """Returns difference between CEA-predicted T04 and target T04"""
             _, _, T_cea = self.Run_CEA(fuel, oxid, FAR)
             return T_cea - T02
     
         # initial guesses for FAR - bracket a reasonable range
         FAR_guess0 = 0.01
-        FAR_guess1 = 10
+        FAR_guess1 = 1
 
-        self.FAR = Secant_Method(residual, FAR_guess0, FAR_guess1, 1e-6, 50)
+        self.FAR = Secant_Method(Residual, FAR_guess0, FAR_guess1, 1e-6, 50)
 
         # get final species/pressure/temp at converged FAR
         spec_pairs, P02, T02 = self.Run_CEA(fuel, oxid, FAR)
 
         # SET EXIT FLOW ---------------------------------
         self.FlowOut.setFlow(
-            T0 = self.T04, P0 = self.P02, FAR = self.FAR,
+            T0 = self.T02, P0 = self.P02, FAR = self.FAR,
             Wfactor = self.Wfactor, Wstream = self.Wstream
         )
 
-    def Run_CEA(self, fuel, oxid, FAR):
+    def Run_CEA(self, FAR):
         """Run CEA and return the species list, pressure, and temperature"""
         problem = CEA.HPProblem(pressure = (self.FlowIn.P0 / 100), massf = True, pressure_units = "bar")
         problem.set_phi(1 / FAR)
-        data = problem.run(fuel, oxid)
+        data = problem.run(self.Get_Mixtures("Jet-A"))
 
         spec = [] # species list
         masses = [] # mass fraction list
